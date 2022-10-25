5c935009e22146d3f32ef7020de6d0b69c5862b6
luhluh-17 <castro.ralph17@gmail.com>
2022-10-25 15:51:05 +0800

    remove initialize

--- lib/challonge/client.rb
+++ lib/challonge/client.rb
@@ -6,26 +6,19 @@ module Challonge
   class Client
     BASE_URL = 'https://api.challonge.com/v1/'
 
-    attr_reader :api_key, :adapter
-
-    def initialize(api_key:, adapter: Faraday.default_adapter)
-      @api_key = api_key
-      @adapter = adapter
-    end
-
-    def connection
+    def self.connection
       @connection ||=
         Faraday.new do |conn|
           conn.url_prefix = BASE_URL
-          conn.params = { api_key: api_key }
+          conn.params = { api_key: ENV['API_KEY'] }
           conn.request :json
           conn.response :json
-          conn.adapter adapter
+          conn.adapter Faraday.default_adapter
           conn.use Faraday::Response::RaiseError
         end
     end
 
-    def call(http_method:, endpoint:, args: {})
+    def self.call(http_method:, endpoint:, args: {})
       response = connection.send(http_method, endpoint, args)
       { code: response.status, status: 'Success', data: response.body }
     rescue Faraday::Error => e
