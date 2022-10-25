0a27b9bd474832e7e2ca4d2d6609539174fb77f4
luhluh-17 <castro.ralph17@gmail.com>
2022-10-25 19:50:12 +0800

    define a method for endpoint

--- lib/challonge/tournament.rb
+++ lib/challonge/tournament.rb
@@ -3,23 +3,29 @@
 module Challonge
   class Tournament
     def self.all(args: {})
-      Challonge::Client.call(http_method: 'get', endpoint: 'tournaments.json', args: args)
+      Challonge::Client.call(http_method: 'get', endpoint: endpoint, args: args)
     end
 
     def self.find_by_id(id:)
-      Challonge::Client.call(http_method: 'get', endpoint: "tournaments/#{id}.json")
+      Challonge::Client.call(http_method: 'get', endpoint: endpoint(id))
     end
 
     def self.create(name:)
-      Challonge::Client.call(http_method: 'post', endpoint: 'tournaments.json', args: { name: name })
+      Challonge::Client.call(http_method: 'post', endpoint: endpoint, args: { name: name })
     end
 
     def self.update(id:, name:)
-      Challonge::Client.call(http_method: 'put', endpoint: "tournaments/#{id}.json", args: { name: name })
+      Challonge::Client.call(http_method: 'put', endpoint: endpoint(id), args: { name: name })
     end
 
     def self.delete(id:)
-      Challonge::Client.call(http_method: 'delete', endpoint: "tournaments/#{id}.json")
+      Challonge::Client.call(http_method: 'delete', endpoint: endpoint(id))
+    end
+
+    def self.endpoint(id: nil)
+      return 'tournaments.json' if id.nil?
+
+      "tournaments/#{id}.json"
     end
   end
 end
